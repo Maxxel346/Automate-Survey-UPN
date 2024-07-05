859866603300556628b4eedbf1e7be1c991cfcb4
Maxxel <108078733+Maxxel346@users.noreply.github.com>
2024-07-05 21:34:55 +0700

    Update SurveyUPN.py

--- SurveyUPN.py
+++ SurveyUPN.py
@@ -1,22 +1,13 @@
 import time
 import os
-import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import threading
 
 def opener(name, path):
-    # chromePath = os.getenv("CHROME_PATH")
-    # chromePath = "E:\chromedriver-win32\chromedriver.exe"
-    # D:\Acer\Music\share\buki\chromedriver_win32\chromedriver.exe
-    # chromePath = "D:\Acer\Music\share\buki\chromedriver_win32\chromedriver.exe"
-    # print(chromePath)
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     profile = path + "\PROFILE" + "\\" + "Profile " + name
@@ -37,21 +28,29 @@ time.sleep(20)
 
 try:    
 
-    # # Log in
-    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "form_login")))
-    # login_form = driver.find_element(By.ID, "form_login")
-    
-    # form_groups = login_form.find_elements(By.CLASS_NAME, "form-group")
-    # if len(form_groups) >= 2:
-    #     username_input = form_groups[0].find_element(By.TAG_NAME, "input")
-    #     password_input = form_groups[1].find_element(By.TAG_NAME, "input")
-        
-    #     # Enter your username and password
-    #     username_input.send_keys("")
-    #     password_input.send_keys("")
-    #     time.sleep(10)
-    #     # Submit the form
-    #     login_form.submit()
+    # Log in
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "form_login")))
+
+    # Find the login form
+    login_form = driver.find_element(By.ID, "form_login")
+
+    # Fill in the login form
+    if login_form is not None:
+        form_groups = login_form.find_elements(By.CLASS_NAME, "form-group")
+        if len(form_groups) >= 2:
+            username_input = form_groups[0].find_element(By.TAG_NAME, "input")
+            password_input = form_groups[1].find_element(By.TAG_NAME, "input")
+            
+            # Enter your username and password
+            username_input.send_keys("fill in your username here")
+            password_input.send_keys("fill in your password here")
+
+            # Wait for a short period to ensure the page loads the captcha and manually solve it
+            # Fell free to adjust the waiting time
+            time.sleep(20)
+
+            # Submit the form
+            login_form.submit()
 
     # Wait for login to complete
     WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, "form_kuisoner-p-0")))
@@ -70,10 +69,6 @@ try:
         p_0_elements = section.find_elements(By.CLASS_NAME, "p-0")
         print(len(p_0_elements))
         for p_0_element in p_0_elements:
-            # if "pt-0" in p_0_element.get_attribute("class"):
-            #     # click third class pt-0
-            #     radio_box = p_0_element.find_elements(By.CLASS_NAME, "pt-0")[2]
-            #     radio_box.click()
             try:
                 radio_box = p_0_element.find_elements(By.CLASS_NAME, "pt-0")[2]
                 radio_button = radio_box.find_element(By.TAG_NAME, "input")
@@ -82,15 +77,6 @@ try:
             except:
                 print("error")
 
-
-        # pt_0_elements = [element for element in p_0_elements if "pt-0" in element.get_attribute("class")]
-        # print(len(pt_0_elements))
-        # if len(pt_0_elements) > 2:
-        #     radio_box = pt_0_elements[2]
-        #     radio_box.click()
-        #     print(radio_box)
-
-        # time.sleep(500)
         # Move to the next section by clicking the second `li` inside a div with class ".actions"
         actions_div = driver.find_element(By.CSS_SELECTOR, ".actions")
         li_elements = actions_div.find_elements(By.TAG_NAME, "li")
@@ -109,6 +95,6 @@ try:
         time.sleep(2)
 
 finally:
+    time.sleep(5)
     # Close the browser after completing the task
-    # driver.quit()
-    time.sleep(100)
+    driver.quit()
